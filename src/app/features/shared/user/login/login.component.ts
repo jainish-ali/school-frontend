6ea970df905630438a000fde9f156e92de7ad9e8
Skylabs <code@skylabstech.com>
2024-11-11 16:36:18 +0530

    login

--- src/app/features/shared/user/login/login.component.ts
+++ src/app/features/shared/user/login/login.component.ts
@@ -3,7 +3,9 @@ import { SwitchButton } from '../../interfaces/switch-button';
 import { BsModalRef, BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
 import { ErrorModalComponent } from '../../components/error-modal/error-modal.component';
 import { TokenService } from 'src/app/features/http-services/token.service';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -11,49 +13,29 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  loginForm!: FormGroup;
   viewType: string = "swatch";
-  spinnerLoading: boolean = false;
-  registerForm!: FormGroup;
-  constructor (private modalService: BsModalService,    private tokenService: TokenService,){}
-  switchBtnConfig: SwitchButton = {
-    leftLabel: "Admin",
-    leftValue: "Admin",
-    rightLabel: "User",
-    rightValue: "user",
-    id: "image-mode",
-  };
-  ngOnInit(){
-    this.setFormValue();
+
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private tokenService: TokenService
+  ) { }
+
+  ngOnInit() {
+    this.setIntialvalue()
   }
-  setFormValue() {
-    this.registerForm = new FormGroup(
-      {
-        email: new FormControl(
-          '',
-          Validators.compose([Validators.required])
-        ),
-        password: new FormControl(
-          '',
-          Validators.compose([Validators.required, Validators.minLength(3)])
-        ),
-      },
-      {}
-    );
+
+  /**setinitial value on login form */
+  setIntialvalue() {
+    this.loginForm = this.fb.group({
+      userName: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
   }
-  onSwitch(data: string) {
-    this.viewType = data;
-    // const anyService = this.owlCar as any;
-    // // console.log("this.owlCar: ", this.owlCar);
-    // const carouselService = anyService.carouselService as CarouselService;
-    // carouselService.register("");
-    // carouselService.refresh();
-    // this.cd.detectChanges();
-  }
-  modalRef?: BsModalRef;
-  bsModalRef?: BsModalRef;
-  onSubmit(formValue : any) {
-    console.log(formValue,'form');
-    
-    this.tokenService.generateToken(formValue);
+
+  /**submit method here for login */
+  submit(formvalue: any) {
+    this.tokenService.generateToken(formvalue)
   }
 }
